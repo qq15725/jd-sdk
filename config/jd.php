e37274cf21efd3696de9cfb0a7b83cfed0ad1701
你手捧鲜花 <157215725@qq.com>
2021-08-05 18:58:52 +0800

    update: default config

--- config/jd.php
+++ config/jd.php
@@ -3,15 +3,10 @@
 return [
     'appkey' => env('JD_APPKEY'),
     'appsecret' => env('JD_APPSECRET'),
-    // 'log' => [
-    //   'default' => 'daily',
-    //  'channels' => [
-    //        'daily' => [
-    //           'driver' => 'daily',
-    //           'path' => app()->storagePath('logs/jd.log'),
-    //           'level' => 'debug',
-    //           'days' => 14
-    //       ]
-    //   ],
-    //],
+    'log' => [
+        'driver' => 'daily',
+        'path' => app()->storagePath('logs/jd.log'),
+        'level' => app()->environment() == 'production' ? 'info' : 'debug',
+        'days' => 3
+    ],
 ];
